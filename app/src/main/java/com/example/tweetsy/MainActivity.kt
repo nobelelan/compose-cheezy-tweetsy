992af623e1a5489285c8e1cd8ecd818274351881
Nobel Elan <nobelelan@gmail.com>
2024-02-07 01:10:08 +0600

    Initial commit

---
+++ app/src/main/java/com/example/tweetsy/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.tweetsy
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tweetsy.api.TweetsyAPI
+import com.example.tweetsy.ui.theme.TweetsyTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var tweetsyAPI: TweetsyAPI
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        GlobalScope.launch {
+            var response = tweetsyAPI.getCategories()
+            Log.d("CHEEZYCODE", response.body()!!.distinct().toString())
+        }
+        setContent {
+            TweetsyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TweetsyTheme {
+        Greeting("Android")
+    }
+}
